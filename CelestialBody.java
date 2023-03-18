4e93087e9fff86b764f7992984974ba50d86c3c1
Yusuf Tahir Aydin <54448013+ttrtahir@users.noreply.github.com>
2023-03-18 13:30:52 +0100

    Add files via upload

--- CelestialBody.java
+++ CelestialBody.java
@@ -8,6 +8,8 @@ public class CelestialBody {
     private double mass;
     private String name;
 
+    private final double GRAVITATIONAL_CONSTANT = 6.6743E-20; // km^3/(kg s^2)
+
     public CelestialBody(String name, double x1, double x2, double x3, double v1, double v2, double v3, double mass){
         this.name = name;
         this.x1 = x1;
@@ -18,6 +20,17 @@ public class CelestialBody {
         this.v3 = v3;
         this.mass = mass;
     }
+
+    // F = G * (m1 * m2) / r^2
+    // Math.atan2() method to separate x and y forces
+//    private double calculateForce(CelestialBody anotherCB){
+//        double d1 = this.x1 - anotherCB.getX1();
+//        double d2 = this.x2 - anotherCB.getX2();
+//        double d3 = this.x3 - anotherCB.getX3();
+//        double distance = Math.sqrt(d1*d1 + d2*d2 + d3*d3);
+//        double force = GRAVITATIONAL_CONSTANT * (this.getMass() * anotherCB.getMass()) / (distance * distance);
+//        return force;
+//    }
 
     public double getX1() {
         return x1;
