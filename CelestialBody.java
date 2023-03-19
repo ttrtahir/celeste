057749a649cd03724174376c332dcbabead2fb79
ttrta <ttrtahir@gmail.com>
2023-03-19 17:42:18 +0100

    - force and forces added, some changes related to data types

--- CelestialBody.java
+++ CelestialBody.java
@@ -1,98 +1,59 @@
 public class CelestialBody {
-    private double x1;
-    private double x2;
-    private double x3;
-    private double v1;
-    private double v2;
-    private double v3;
+    private double[] x;
+    private double[] v;
     private double mass;
-    private String name;
+    private double totalForce;
+    private double[] forces;
+    private double[] acceleration;
 
-    private final double GRAVITATIONAL_CONSTANT = 6.6743E-20; // km^3/(kg s^2)
-
-    public CelestialBody(String name, double x1, double x2, double x3, double v1, double v2, double v3, double mass){
-        this.name = name;
-        this.x1 = x1;
-        this.x2 = x2;
-        this.x3 = x3;
-        this.v1 = v1;
-        this.v2 = v2;
-        this.v3 = v3;
+    public CelestialBody(double[] x, double[] v, double mass){
+        this.x = x;
+        this.v = v;
         this.mass = mass;
     }
 
-    // F = G * (m1 * m2) / r^2
-    // Math.atan2() method to separate x and y forces
-//    private double calculateForce(CelestialBody anotherCB){
-//        double d1 = this.x1 - anotherCB.getX1();
-//        double d2 = this.x2 - anotherCB.getX2();
-//        double d3 = this.x3 - anotherCB.getX3();
-//        double distance = Math.sqrt(d1*d1 + d2*d2 + d3*d3);
-//        double force = GRAVITATIONAL_CONSTANT * (this.getMass() * anotherCB.getMass()) / (distance * distance);
-//        return force;
-//    }
-
-    public double getX1() {
-        return x1;
+    public double[] getX() {
+        return x;
     }
 
-    public void setX1(double x1) {
-        this.x1 = x1;
+    public void setX(double[] x) {
+        this.x = x;
     }
 
-    public double getX2() {
-        return x2;
+    public double[] getV() {
+        return v;
     }
 
-    public void setX2(double x2) {
-        this.x2 = x2;
+    public void setV(double[] v) {
+        this.v = v;
     }
 
-    public double getX3() {
-        return x3;
+    public double getTotalForce() {
+        return totalForce;
     }
 
-    public void setX3(double x3) {
-        this.x3 = x3;
+    public void setTotalForce(double totalForce) {
+        this.totalForce = totalForce;
     }
 
-    public double getV1() {
-        return v1;
+    public double[] getForces() {
+        return forces;
     }
 
-    public void setV1(double v1) {
-        this.v1 = v1;
+    public void setForces(double[] forces) {
+        this.forces = forces;
     }
 
-    public double getV2() {
-        return v2;
+    public double[] getAcceleration() {
+        return acceleration;
     }
 
-    public void setV2(double v2) {
-        this.v2 = v2;
-    }
-
-    public double getV3() {
-        return v3;
-    }
-
-    public void setV3(double v3) {
-        this.v3 = v3;
+    public void setAcceleration(double[] acceleration) {
+        this.acceleration = acceleration;
     }
 
     public double getMass() {
         return mass;
     }
 
-    public void setMass(double mass) {
-        this.mass = mass;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
