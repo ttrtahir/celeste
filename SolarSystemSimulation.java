fdf3227230eb9b6a8a4c005c7fea39c8cfc0bfe4
Michal Pavlíček (linux) <michaelg.pavlicek@gmail.com>
2023-03-20 13:01:28 +0100

    Test

--- SolarSystemSimulation.java
+++ SolarSystemSimulation.java
@@ -5,16 +5,16 @@ import javax.swing.JPanel;
 
 public class SolarSystemSimulation extends JPanel {
 
-    //JPanel
+    // JPanel
     private static final int FRAME_WIDTH = 1980;
     private static final int FRAME_HEIGHT = 1080;
 
-    //Sun
+    // Sun
     private static final double SUN_RADIUS = 50;
-    private static final int SUN_X = FRAME_WIDTH/2;
-    private static final int SUN_Y = FRAME_HEIGHT/2;
+    private static final int SUN_X = FRAME_WIDTH / 2;
+    private static final int SUN_Y = FRAME_HEIGHT / 2;
 
-    //Planets
+    // Planets
     private static final float EARTH_RADIUS = 10;
     private static final float EARTH_DISTANCE = 100;
     private static final float JUPYTER_RADIUS = 30;
@@ -31,31 +31,35 @@ public class SolarSystemSimulation extends JPanel {
 
     public void paintComponent(Graphics g) {
 
-        //Background
+        // Background
         super.paintComponent(g);
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, getWidth(), getHeight());
 
         // Draw the Sun
         g.setColor(Color.YELLOW);
-        g.fillOval((int) (system.getCelestialBody()[0].getX()[0] + FRAME_WIDTH/2 - SUN_RADIUS), (int) (system.getCelestialBody()[0].getX()[1] + FRAME_HEIGHT/2 - (int)SUN_RADIUS), (int)(2*SUN_RADIUS), (int)(2*SUN_RADIUS));
+        g.fillOval((int) (system.getCelestialBody()[0].getX()[0] + FRAME_WIDTH / 2 - SUN_RADIUS),
+                (int) (system.getCelestialBody()[0].getX()[1] + FRAME_HEIGHT / 2 - (int) SUN_RADIUS),
+                (int) (2 * SUN_RADIUS), (int) (2 * SUN_RADIUS));
 
         // Draw the earth's orbit, OLD
-//        g.setColor(Color.WHITE);
-//        int earthOrbitSize = (int)EARTH_DISTANCE*2;
-//        g.drawOval(SUN_X - earthOrbitSize / 2, SUN_Y - earthOrbitSize / 2, earthOrbitSize, earthOrbitSize);
+        // g.setColor(Color.WHITE);
+        // int earthOrbitSize = (int)EARTH_DISTANCE*2;
+        // g.drawOval(SUN_X - earthOrbitSize / 2, SUN_Y - earthOrbitSize / 2,
+        // earthOrbitSize, earthOrbitSize);
 
         // Draw the MERCURY NOT EARTH
-//        double velocityX =
-        double x = FRAME_WIDTH/2 + system.getCelestialBody()[1].getX()[0]/300000;
-        double y = FRAME_HEIGHT/2 + system.getCelestialBody()[1].getX()[1]/300000;
+        // double velocityX =
+        double x = FRAME_WIDTH / 2 + system.getCelestialBody()[1].getX()[0] / 300000;
+        double y = FRAME_HEIGHT / 2 + system.getCelestialBody()[1].getX()[1] / 300000;
         g.setColor(Color.BLUE);
-        g.fillOval((int)x - (int)EARTH_RADIUS, (int)y - (int)EARTH_RADIUS, (int)(2*EARTH_RADIUS), (int)(2*EARTH_RADIUS));
+        g.fillOval((int) x - (int) EARTH_RADIUS, (int) y - (int) EARTH_RADIUS, (int) (2 * EARTH_RADIUS),
+                (int) (2 * EARTH_RADIUS));
 
         // Increment angle
-//        angle += ANGLE_INCREMENT;
-//        angleJP += ANGLE_INCREMENT/2;
-//        angleTitan += ANGLE_INCREMENT;
+        // angle += ANGLE_INCREMENT;
+        // angleJP += ANGLE_INCREMENT/2;
+        // angleTitan += ANGLE_INCREMENT;
     }
 
     public static void main(String[] args) {
@@ -66,6 +70,7 @@ public class SolarSystemSimulation extends JPanel {
         frame.add(panel);
         frame.setVisible(true);
 
+        System.out.println("test");
         // Repaint the panel every 10 milliseconds
         while (true) {
             panel.repaint();
