efe1985172db31e009ed0f120634cdcd2b5b04e7
PalomaJoon <115260038+PalomaJoon@users.noreply.github.com>
2023-03-22 13:37:14 +0100

    Remove ö

--- Rocket.java
+++ Rocket.java
@@ -7,82 +7,82 @@ import java.awt.*;
 
 public class Rocket extends JPanel {
 
-    private int xCoördinate;
-    private int yCoördinate;
-    private int width;
-    private int height;
+	private int xCoordinate;
+	private int yCoordinate;
+	private int width;
+	private int height;
 
-    public Rocket(int xCoördinate, int yCoördinate, int width, int height) {
-        this.xCoördinate = xCoördinate;
-        this.yCoördinate = yCoördinate;
-        this.width = width;
-        this.height = height;
-    }
+	public Rocket(int xCoordinate, int yCoordinate, int width, int height) {
+		this.xCoordinate = xCoordinate;
+		this.yCoordinate = yCoordinate;
+		this.width = width;
+		this.height = height;
+	}
 
-    public void paintComponent(Graphics g) {
+	public void paintComponent(Graphics g) {
 
-        Color darkRed = new Color(180, 0, 0);
-        Graphics2D g2 = (Graphics2D) g;
-        g2.scale(0.3, 0.3);
+		Color darkRed = new Color(180, 0, 0);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.scale(0.3, 0.3);
 
-        // Top
-        g.setColor(Color.WHITE);
-        g.fillPolygon(new int[] { xCoördinate - 3, xCoördinate + 5, xCoördinate + 13 },
-                new int[] { yCoördinate, yCoördinate - 25, yCoördinate }, 3);
+		// Top
+		g.setColor(Color.WHITE);
+		g.fillPolygon(new int[] { xCoordinate - 3, xCoordinate + 5, xCoordinate + 13 },
+				new int[] { yCoordinate, yCoordinate - 25, yCoordinate }, 3);
 
-        // first block
-        g.setColor(darkRed);
-        g.fillRect(xCoördinate, yCoördinate, width, height);
+		// first block
+		g.setColor(darkRed);
+		g.fillRect(xCoordinate, yCoordinate, width, height);
 
-        // second block
-        g.setColor(Color.WHITE);
-        g.fillRect(xCoördinate, yCoördinate + 20, width, height - 10);
+		// second block
+		g.setColor(Color.WHITE);
+		g.fillRect(xCoordinate, yCoordinate + 20, width, height - 10);
 
-        // third block
-        g.setColor(darkRed);
-        g.fillRect(xCoördinate, yCoördinate + 30, width, height);
+		// third block
+		g.setColor(darkRed);
+		g.fillRect(xCoordinate, yCoordinate + 30, width, height);
 
-        // middelpart
-        g.setColor(Color.WHITE);
-        g.fillPolygon(new int[] { xCoördinate - 13, xCoördinate - 3, xCoördinate - 3 },
-                new int[] { yCoördinate + 70, yCoördinate + 50, yCoördinate + 70 }, 3);
-        g.fillPolygon(new int[] { xCoördinate + 23, xCoördinate + 13, xCoördinate + 13 },
-                new int[] { yCoördinate + 70, yCoördinate + 50, yCoördinate + 70 }, 3);
-        g.fillRect(xCoördinate, yCoördinate + 50, width, height);
+		// middelpart
+		g.setColor(Color.WHITE);
+		g.fillPolygon(new int[] { xCoordinate - 13, xCoordinate - 3, xCoordinate - 3 },
+				new int[] { yCoordinate + 70, yCoordinate + 50, yCoordinate + 70 }, 3);
+		g.fillPolygon(new int[] { xCoordinate + 23, xCoordinate + 13, xCoordinate + 13 },
+				new int[] { yCoordinate + 70, yCoordinate + 50, yCoordinate + 70 }, 3);
+		g.fillRect(xCoordinate, yCoordinate + 50, width, height);
 
-        // block
-        g.setColor(darkRed);
-        g.fillRect(xCoördinate - 10, yCoördinate + 70, width + 18, height + 10);
+		// block
+		g.setColor(darkRed);
+		g.fillRect(xCoordinate - 10, yCoordinate + 70, width + 18, height + 10);
 
-        // block
-        g.setColor(Color.WHITE);
-        g.fillRect(xCoördinate - 10, yCoördinate + 100, width + 18, height + 10);
+		// block
+		g.setColor(Color.WHITE);
+		g.fillRect(xCoordinate - 10, yCoordinate + 100, width + 18, height + 10);
 
-        // last part
-        g.setColor(darkRed);
-        g.fillPolygon(new int[] { xCoördinate - 20, xCoördinate - 10, xCoördinate - 10 },
-                new int[] { yCoördinate + 160, yCoördinate + 130, yCoördinate + 160 }, 3);
-        g.fillPolygon(new int[] { xCoördinate + 35, xCoördinate + 25, xCoördinate + 25 },
-                new int[] { yCoördinate + 160, yCoördinate + 130, yCoördinate + 160 }, 3);
-        g.fillRect(xCoördinate - 10, yCoördinate + 130, width + 20, height + 10);
-    }
+		// last part
+		g.setColor(darkRed);
+		g.fillPolygon(new int[] { xCoordinate - 20, xCoordinate - 10, xCoordinate - 10 },
+				new int[] { yCoordinate + 160, yCoordinate + 130, yCoordinate + 160 }, 3);
+		g.fillPolygon(new int[] { xCoordinate + 35, xCoordinate + 25, xCoordinate + 25 },
+				new int[] { yCoordinate + 160, yCoordinate + 130, yCoordinate + 160 }, 3);
+		g.fillRect(xCoordinate - 10, yCoordinate + 130, width + 20, height + 10);
+	}
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setSize(500, 500);
-        frame.setSize(1000, 500);
-        frame.setTitle("Rocket");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
-        // frame.setResizable(false);
-        // frame.setVisible(true);
-        Rocket panel = new Rocket(50, 50, 16, 20);
-        frame.add(panel);
-        // Rocket panel2 = new Rocket(50, 50, 15, 20);
-        panel.setBounds(200, 50, 200, 400);
-        // frame.add(panel2);
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setSize(500, 500);
+		frame.setSize(1000, 500);
+		frame.setTitle("Rocket");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		// frame.setResizable(false);
+		// frame.setVisible(true);
+		Rocket panel = new Rocket(50, 50, 16, 20);
+		frame.add(panel);
+		// Rocket panel2 = new Rocket(50, 50, 15, 20);
+		panel.setBounds(200, 50, 200, 400);
+		// frame.add(panel2);
 
-        frame.setVisible(true);
+		frame.setVisible(true);
 
-    }
+	}
 }
