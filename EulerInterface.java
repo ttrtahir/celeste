d97344c1309f98eda7f744dc6e81d8b69803c886
vthu263 <113668646+vthu263@users.noreply.github.com>
2023-04-20 12:33:06 +0200

    incompleted code clean

--- EulerInterface.java
+++
@@ -1,7 +0,0 @@
-public interface EulerInterface {
-    public void calculateForce();
-    public double calculateDistanceBetweenCelestials(double[] x1, double[] x2);
-    public void updateAcceleration();
-    public void updatePosition();
-    public void updateVelocity();
-}
