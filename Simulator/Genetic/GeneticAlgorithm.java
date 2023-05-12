ea65ac925ad7dbd28514c25759ccde9f78eecd4b
Yusuf Tahir Aydin <ttrtahir@gmail.com>
2023-05-12 09:58:47 +0200

    genetic algorithm archaic version

---
+++ Simulator/Genetic/GeneticAlgorithm.java
@@ -0,0 +1,32 @@
+package Simulator.Genetic;
+
+import Simulator.Vector3;
+
+public class GeneticAlgorithm{
+
+    // to determine the initial velocity and position on the earth
+
+    private double mutationRate = 0.1;
+    private int population = 100;
+    private int generation = 100;
+    
+    public void 
+
+    for(){
+        run calculations 
+    }
+    
+    
+    class Individual{
+        // for each individual initial
+        private Vector3 position = new Vector3(1,1,1);
+        private int velocity = 1;
+        private double fitness = 0;
+        
+        Individual(){
+
+        }
+    }
+
+
+}
