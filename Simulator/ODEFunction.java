3e06888764c9be9a6fcf389a4665bbb4c5b79ae2
Yusuf Tahir Aydin <ttrtahir@gmail.com>
2023-05-16 13:41:52 +0200

    Newtons formula fix

--- Simulator/ODEFunction.java
+++ Simulator/ODEFunction.java
@@ -26,7 +26,7 @@ public class ODEFunction implements IODEFunction {
         //get dx = v
 
         for(int i = 0; i < nBodies; i++){
-            Vector3 velocity = (Vector3) ((State) y).getVelocity(i);  //suspicious
+            Vector3 velocity = (Vector3) ((State) y).getVelocity(i);  
             acceleration.addVelocity(i, velocity);
         }
         // get dx = acceleration
@@ -34,10 +34,12 @@ public class ODEFunction implements IODEFunction {
             double distance = 0;
             for (int j = 0; j < nBodies; j++) {
                 if (i != j) {
-                    // calculate the distance
+                    // calculate the distance 
+                    // a(i) = G * m(j) * pos(i) - pos(j) / (euler distance(pos(i) - pos(j))) ^ 3 
                     distance = Math.pow(((State) y).getPosition(i).euclideanDist(((State) y).getPosition(j)), 3);
-                    Vector3 tempAcc = (Vector3) (((State) y).getPosition(j).subtract(((State) y).getPosition(i)))
-                            .multiply(1 / distance);
+                    Vector3 positionalDifference = (Vector3)((((State) y).getPosition(j).subtract(((State) y).getPosition(i))));
+                    double aThing = G*CelestialBody.celestialBodies[j].getMass()/distance;
+                    Vector3 tempAcc = (Vector3) (positionalDifference.multiply(aThing));
                     // add the temporary acceleration to total acceleration of a planet
                     acceleration.addAcceleration(i, tempAcc);
                 }
@@ -46,5 +48,4 @@ public class ODEFunction implements IODEFunction {
         //System.out.println("Acceleration after update " + acceleration.toString());
         return acceleration;
     }
-
 }
