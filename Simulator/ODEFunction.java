0ec89f12589c90eccb3fc824fc7c214d7226711c
Yusuf Tahir Aydin <ttrtahir@gmail.com>
2023-05-17 14:12:30 +0200

    fixing the comments

--- Simulator/ODEFunction.java
+++ Simulator/ODEFunction.java
@@ -4,6 +4,9 @@ import Interface.IAccelerationRate;
 import Interface.IODEFunction;
 import Interface.IState;
 
+/**
+ * A class contains a method for Newton's gravitational calculations
+ */
 public class ODEFunction implements IODEFunction {
     // Gravitational constant
     public static final double G = 6.6743E-20;
@@ -21,31 +24,28 @@ public class ODEFunction implements IODEFunction {
     @Override
     public IAccelerationRate call(double t, IState y) {
         acceleration.initialize(nBodies);
-        //System.out.println("acceleration before update " + acceleration.toString());
-
-        //get dx = v
 
         for(int i = 0; i < nBodies; i++){
             Vector3 velocity = (Vector3) ((State) y).getVelocity(i);  
             acceleration.addVelocity(i, velocity);
         }
-        // get dx = acceleration
+
         for (int i = 0; i < nBodies; i++) {
             double distance = 0;
             for (int j = 0; j < nBodies; j++) {
                 if (i != j) {
                     // calculate the distance 
-                    // a(i) = G * m(j) * pos(i) - pos(j) / (euler distance(pos(i) - pos(j))) ^ 3 
                     distance = Math.pow(((State) y).getPosition(i).euclideanDist(((State) y).getPosition(j)), 3);
                     Vector3 positionalDifference = (Vector3)((((State) y).getPosition(j).subtract(((State) y).getPosition(i))));
-                    double aThing = G*CelestialBody.celestialBodies[j].getMass()/distance;
-                    Vector3 tempAcc = (Vector3) (positionalDifference.multiply(aThing));
+
+                    // a(i) = G * m(j) * pos(i) - pos(j) / (euler distance(pos(i) - pos(j))) ^ 3 
+                    Vector3 tempAcc = (Vector3) (positionalDifference.multiply(G*CelestialBody.celestialBodies[j].getMass()/distance));
+                    
                     // add the temporary acceleration to total acceleration of a planet
                     acceleration.addAcceleration(i, tempAcc);
                 }
             }
         }
-        //System.out.println("Acceleration after update " + acceleration.toString());
         return acceleration;
     }
 }
