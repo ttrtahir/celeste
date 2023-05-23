a9439a5612435de74631349f8ed099cb1819b352
PalomaJoon <115260038+PalomaJoon@users.noreply.github.com>
2023-05-23 12:42:28 +0200

    Commeting

--- Simulator/RK2.java
+++ Simulator/RK2.java
@@ -3,8 +3,21 @@ package Simulator;
 import Interface.IODEFunction;
 import Interface.IState;
 
+/**
+ * Class modifies the step method of the ODESolver class to calculate the state
+ * after applying RK4
+ */
 public class RK2 extends ODESolver {
 
+    /**
+     * calculating one step
+     *
+     * @param f simulator.ODEFunction implementing Newton's law
+     * @param t time
+     * @param y state
+     * @param h step size
+     * @return new state of the system after one update step
+     */
     @Override
     public IState step(IODEFunction f, double t, IState y, double h) {
         AccelerationRate k1 = (AccelerationRate) f.call(t, y);
