1e76ea7e4323741f442d3dc116678eb695ddab07
marianboss21 <115264653+marianboss21@users.noreply.github.com>
2023-06-14 16:48:17 +0200

    Create LandingModule.java

---
+++ app/src/main/java/celeste/Simulator/CelestialBodies/LandingModule.java
@@ -0,0 +1,51 @@
+package celeste.Simulator.CelestialBodies;
+
+import celeste.Simulator.Solvers.*;
+import celeste.Simulator.State;
+import celeste.Interface.IODEFunction;
+import celeste.Interface.IODESolver;
+import celeste.Interface.IState;
+
+public class LandingModule {
+    
+    private IODESolver solver;
+    private IODEFunction function;
+    private State currentState;
+
+    public LandingModule(IODEFunction function) {
+        this.solver = new RK2();
+        this.function = function;
+        this.currentState = new State(); 
+    }
+
+    public void stimulateMotion(double h, double timefinal) {
+        // timefinal - total time of simulation, h - step size
+        
+        IState[] states = solver.solve(function, currentState, h, timefinal);
+        
+        for (IState state : states) {
+            //Resulting states
+            System.out.println(state.toString());
+        }
+    }
+    public void openLoopController() {
+        //TO-DO
+    }
+
+  
+    public void feedbackController() {
+        //TO-DO
+    }
+
+    
+    public void windModel() {
+        //TO-DO
+    }
+
+    
+    public boolean checkLanding() {
+        //TO-DO
+        return false;
+    }
+}
+
