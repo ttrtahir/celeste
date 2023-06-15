2e36fd9ee2f4e8bf741d71afa3db8886a3885236
marianboss21 <115264653+marianboss21@users.noreply.github.com>
2023-06-15 14:14:35 +0200

    Added PIDControler skeleton.

--- app/src/main/java/celeste/Simulator/CelestialBodies/LandingModule.java
+++ app/src/main/java/celeste/Simulator/CelestialBodies/LandingModule.java
@@ -11,11 +11,17 @@ public class LandingModule {
     private IODESolver solver;
     private IODEFunction function;
     private State currentState;
+    private PIDController[] controllers;
 
     public LandingModule(IODEFunction function) {
         this.solver = new RK2();
         this.function = function;
-        this.currentState = new State(); 
+        this.currentState = new State();
+
+        this.controllers = new PIDController[3];  // assuming 3D space
+        for (int i = 0; i < 3; i++) {
+            this.controllers[i] = new PIDController(0,0,0);
+        }
     }
 
     public void stimulateMotion(double h, double timefinal) {
@@ -28,28 +34,34 @@ public class LandingModule {
             System.out.println(state.toString());
         }
     }
+
     public double[] openLoopController(double t) {
-        
-        double[] control = new double[3]; 
+        // Control values for x, y, z are pre-defined or computed based on time t
+        double[] control = new double[3];
+        control[0] = 0;/* control for x */
+        control[1] = 0;/* control for y */
+        control[2] = 0;/* control for z */
         return control;
     }
 
-  
-    public double[] feedbackController(State currentState, State desiredState) {
+    public double[] feedbackController(State currentState, State desiredState, double dt) {
+        // Control is computed based on the difference between current and desired state
+        double[] control = new double[3];
 
-        double[] control = new double[3]; 
+        for (int i = 0; i < 3; i++) {
+            controllers[i].setSetPoint(desiredState.state[i][0]); // Assuming state[i][0] is position
+            control[i] = controllers[i].calculate(currentState.state[i][0]); // Feed current position to PID
+        }
+
         return control;
     }
 
-    
     public void windModel() {
         //TO-DO
     }
 
-    
     public boolean checkLanding() {
         //TO-DO
         return false;
     }
 }
-
